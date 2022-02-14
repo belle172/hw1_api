948f926c319788e9abe453f3286d60b929863918
belle172 <belle172@umn.edu>
2022-02-13 18:04:47 -0600

    change port to 7998

--- server.py
+++ server.py
@@ -66,4 +66,4 @@ def removeTask():
    return jsonify({"tasks":task});
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=7651, debug = True)
+   app.run(host="0.0.0.0", port=7998, debug = True)
